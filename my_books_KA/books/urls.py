d69a02edf638d717d5da966aaf26692119b853c2
anitasusnanita <anitasusnanita@gmail.com>
2026-04-21 17:27:15 -0700

    Arregle el proyecto con el profe

--- my_books_KA/books/urls.py
+++ my_books_KA/books/urls.py
@@ -4,5 +4,5 @@ from .views import home, detail
 
 urlpatterns = [
     path('', home, name='home'),
-    path('detail_my_book_k/<int:id>/', detail, name='detail_my_book_k'),
+    path('detail_book/<int:id>/', detail, name='detail_book'),
 ]
